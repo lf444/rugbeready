16b6909c12956e874c81e52c2e4c35fa974685c2
ANTWI Daniel <daniel.jr014@gmail.com>
2021-05-17 16:50:05 +0200

    rebase

--- tests/unit/homehead.spec.js
+++ tests/unit/homehead.spec.js
@@ -1,28 +1,19 @@
 import HomeHead from '@/components/HomeHead.vue'
 import { text } from '@fortawesome/fontawesome-svg-core';
-import { shallowMount,createLocalVue,RouterLinkStub } from '@vue/test-utils'
-//import VueRouter from 'vue-router'
-import Vue from 'vue'
+import { shallowMount } from '@vue/test-utils'
+
 describe("HomeHead.vue",()=> {
-    
-    Vue.config.silent = true;
-
     let wrapper;
- /**const localVue = createLocalVue();
-    localVue.use(VueRouter);*/
-
     beforeEach(() => {
         wrapper = shallowMount(HomeHead,{
-            stubs: { RouterLink: RouterLinkStub, },
+
         })
     })
     it("regarde si l'onglet equipes existe",()=>{
-        expect(wrapper.find("#Equipes").exists()).toBe(true);
-        expect(wrapper.find('#Equipes').text()).toBe("Equipes")
+        expect(wrapper.findAll("v-list-item-title").at(0).text()).toBe("Equipes")
     })
     it("regarde si l'onglet calendrier existe",()=>{
-        expect(wrapper.find("#Calendrier").exists()).toBe(true);
-        expect(wrapper.find('#Calendrier').text()).toBe("Calendrier")
+        expect(wrapper.findAll("v-list-item-title").at(1).text()).toBe("Calendrier")
     })
 
     // it("regarde si lorsqu'on clique sur messagerie, on va la-bas",()=>{
