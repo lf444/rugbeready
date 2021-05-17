8001601cbe2c1ea9161fd994a1a5072033ff74cf
anthonyperret <anthony.perret.contact@gmail.com>
2021-05-17 14:31:42 +0200

    fix test

--- tests/unit/homehead.spec.js
+++ tests/unit/homehead.spec.js
@@ -15,9 +15,6 @@ describe("HomeHead.vue",()=> {
     it("regarde si l'onglet calendrier existe",()=>{
         expect(wrapper.findAll("v-list-item-title").at(1).text()).toBe("Calendrier")
     })
-    // it("regarde si l'onglet messagerie existe",()=>{
-    //     expect(wrapper.findAll("v-list-item-title").at(2).text()).toBe("Messagerie")
-    // })
 
     it("regarde si lorsqu'on clique sur messagerie, on va la-bas",()=>{
         wrapper.findAll("router-link").at(2).trigger("click")
